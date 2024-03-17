462c4e5d0c2d21bff2a132fa53f4113b4b6a087f
Dominique Clark <melaydominique@gmail.com>
2024-03-17 12:26:28 -0400

    solved, beats per meassure

--- mystery_int.py
+++ mystery_int.py
@@ -1,3 +1,4 @@
+# FIND SUM OF ALL NUMBERS BETWEEN 0 AND MYSTERY_INT
 def total_sum(myster_int):
     count = 0
     absolute_num = abs(myster_int)
@@ -7,7 +8,24 @@ def total_sum(myster_int):
         count = count * -1
     return count
 
-mystery_int = 4
-print(total_sum(mystery_int))
+# mystery_int = 4
+# print(total_sum(mystery_int))
+
+# BEATS PER MEASURE
+def total_beats(beats, measure):
+    last_beats = []
+    for num in range(1, beats + 1):
+        if num > 1:
+            last_beats.append(num)
+        print(num)
+    for num in range(2, measure + 1):
+        print(num)
+        for beat in range(len(last_beats)):
+            print(last_beats[beat])
+
+beats = 4
+measure = 5
+print(total_beats(beats, measure))
+
 
 
