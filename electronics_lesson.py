b9bb511bb43c06deb28551331356298cf4ba9b07
Dominique Clark <melaydominique@gmail.com>
2024-03-19 14:06:55 -0400

    electronics lesson complete with questions

--- electronics_lesson.py
+++ electronics_lesson.py
@@ -1,12 +1,10 @@
-# print('hello world')
-
 class Device:
     def __init__(self, brand, year, model):
         self.brand = brand
         self.year = year
         self.model = model
     def display_info(self):
-        print(f'Device: {self.brand} {self.model}, {self.year}')
+        print(f'- Device: {self.brand} {self.model}, {self.year}')
 
 class Phone(Device):
     def __init__(self, brand, year, model, carrier):
@@ -33,8 +31,74 @@ class Laptop(Device):
         self.storage -= amount
         print(f'Your {self.model} has {self.storage}GB left')
 
-device1 = Laptop('Apple', 2023, 'MacBook', 500)
-device2 = Phone('Apple', 2014, 'iPhone 13', 'T-Mobile')
+class Headphones(Device):
+    def __init__(self, brand, year, model, songs):
+        super().__init__(brand, year, model)
+        self.songs = songs
+    def display_info(self):
+        print('HEADPHONE OVERRIDE')
+    def add_song(self, song):
+        self.songs.append(song)
+    def display_playlist(self):
+        print('Playlist:')
+        for song in self.songs:
+            print(f'- {song}')
+
+
+# CREATE TWO DEVICES - LAPTOP AND PHONE
+
+# DISPLAY INFORMATION ABOUT YOUR LAPTOP
+        
+# DISPLAY INFORMATION ABOUT YOUR PHONE
+            
+# DISPLAY INFORMATION ABOUT YOUR HEADPHONES
+        
+# CALL A FRIEND USING YOUR PHONE
+        
+# USE STORAGE SPACE ON YOUR LAPTOP
+
+# PRINT YOUR HEADPHONES PLAYLIST
+            
+# ADD A SONG TO YOUR PLAYLIST AND PRINT AGAIN
+        
+# PAY YOUR PHONE BILL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# device1 = Laptop('Apple', 2023, 'MacBook', 500)
+# device2 = Phone('Apple', 2014, 'iPhone 13', 'T-Mobile')
+# device3 = Headphones('Bose', 2022, 'QC45', ['See Through', 'Kills', 'War'])
 # device2.display_info()
 # device2.make_call()
 # device1.use_storage(100)
